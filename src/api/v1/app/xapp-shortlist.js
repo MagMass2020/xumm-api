bc9133eab30889d6eca41030d3948840da62579c
Wietse Wind <mail@wietse.com>
2021-01-12 12:10:14 +0100

    Update XAPP Shortlist icon path

--- src/api/v1/app/xapp-shortlist.js
+++ src/api/v1/app/xapp-shortlist.js
@@ -1,36 +1,30 @@
 const getUserDevices = require('~api/v1/internal/get-user-devices')
 
 module.exports = async (req, res) => {
-  const apps = [
-    {
-      title: 'Tangem Backup',
-      icon: 'https://xapps.xumm.app/assets/app-icons/app1.png',
-      location: 'https://xapps.xumm.app/account:rxxxx/version:1.0.1/locale:en-US/app:xumm.tangem/',
-    },
-    {
-      title: 'Multi Sign',
-      icon: 'https://xapps.xumm.app/assets/app-icons/app2.png',
-      location: 'https://xapps.xumm.app/account:rxxxx/version:1.0.1/locale:en-US/app:xumm.multisign/',
-    },
-    {
-      title: 'Create Escrow',
-      icon: 'https://xapps.xumm.app/assets/app-icons/app3.png',
-      location: 'https://xapps.xumm.app/account:rxxxx/version:1.0.1/locale:en-US/app:xumm.escrow/',
-    },
-    {
-      title: 'Sample Hook',
-      icon: 'https://xapps.xumm.app/assets/app-icons/app4.png',
-      location: 'https://xapps.xumm.app/account:rxxxx/version:1.0.1/locale:en-US/app:xumm.hook/',
-    }
-  ]
-
   try {
+    const baseUrl = 'https://xapps.xumm.app/'
+    const envUrl = `account:${req.params.account}/version:${req.params.version}/locale:${req.params.locale}/`
+
+    const appList = {
+      'xumm.tangem': 'Tangem Backup',
+      'xumm.multisign': 'Multi Sign',
+      'xumm.escrow': 'Create Escrow',
+      'xumm.hook': 'Sample Hook'
+    }
+
+    const apps = Object.keys(appList).map(k => {
+      return {
+        title: appList[k],
+        icon: 'https://xumm.app/assets/icons/apps/' + k + '.png',
+        location: baseUrl + envUrl + 'app:' + k
+      }
+    })
+
     return res.json({
       ...(req.params),
       apps,
-      moreUrl: `https://xapps.xumm.app/account:${req.params.account}/version:${req.params.version}/locale:${req.params.locale}/`
+      moreUrl: baseUrl + envUrl
     })
-    // return res.json(devices)
   } catch (e) {
     res.handleError(e)
   }
