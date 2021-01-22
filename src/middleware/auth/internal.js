27cd1bff1a755794f3f0225f53fa43d969935ff9
Wietse Wind <mail@wietse.com>
2021-01-22 16:29:12 +0100

    Internal API’s: allow auth bypass by API route definition

--- src/middleware/auth/internal.js
+++ src/middleware/auth/internal.js
@@ -1,8 +1,8 @@
 const log = require('~src/handler/log')('app:internal-api')
 
-module.exports = async (expressApp, req, res) => {
+module.exports = async (expressApp, req, res, apiDetails) => {
   // log('<< API: INTERNAL MIDDLEWARE >>')
-  if (req.ipTrusted) {
+  if (req.ipTrusted || !apiDetails.auth) {
     // log(req.headers.authorization)
     return
   }
