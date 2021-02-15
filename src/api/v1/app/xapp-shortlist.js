f2312f21cd736b6b249380de93a8b2177a6de53f
Wietse Wind <mail@wietse.com>
2021-02-15 16:01:52 +0100

    Fix xApp shortlist routing

--- src/api/v1/app/xapp-shortlist.js
+++ src/api/v1/app/xapp-shortlist.js
@@ -1,9 +1,10 @@
-const getUserDevices = require('~api/v1/internal/get-user-devices')
+const log = require('~src/handler/log')('app:xapp:shortlist')
+// const getUserDevices = require('~api/v1/internal/get-user-devices')
 
 module.exports = async (req, res) => {
   try {
     const baseUrl = 'https://xapps.xumm.app/'
-    const envUrl = `account:${req.params.account}/version:${req.params.version}/locale:${req.params.locale}/`
+    // const envUrl = `account:${req.params.account}/version:${req.params.version}/locale:${req.params.locale}/`
 
     const appList = {
       'xumm.tangem': 'Tangem Backup',
@@ -12,24 +13,27 @@ module.exports = async (req, res) => {
       'xumm.hook': 'Sample Hook'
     }
 
-    const customUrl = {
-      wietse: 'https://thong-clients-brazil-locked.trycloudflare.com/detect/xapp:xumm.tangem'
-    }
+    log('DeviceId', req.__auth.device.id)
+
+    // const customUrl = {
+    //   wietse: 'https://thong-clients-brazil-locked.trycloudflare.com/detect/xapp:xumm.tangem'
+    // }
 
     const apps = Object.keys(appList).map(k => {
       return {
         title: appList[k],
         icon: 'https://xumm.app/assets/icons/apps/' + k + (k.split('.').length > 1 ? '.png' : '.jpg'),
-        location: Object.keys(customUrl).indexOf(k) > -1
-          ? customUrl[k]
-          : baseUrl + envUrl + 'app:' + k
+        identifier: k
+        // location: Object.keys(customUrl).indexOf(k) > -1
+        //   ? customUrl[k]
+        //   : baseUrl + envUrl + 'app:' + k
       }
     })
 
     return res.json({
       ...(req.params),
       apps,
-      moreUrl: baseUrl + envUrl
+      moreUrl: baseUrl // + envUrl
     })
   } catch (e) {
     res.handleError(e)
