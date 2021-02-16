8b6df769c93fbe89b888802d4de1fbbc1d072a11
Wietse Wind <mail@wietse.com>
2021-02-16 12:50:14 +0100

    xApp error handle + UA check

--- src/handler/web-misc/xapp.js
+++ src/handler/web-misc/xapp.js
@@ -1,42 +1,59 @@
 const log = require('~src/handler/log')('app:web:xapp-redir')
 
 module.exports = async (req, res, next) => {
-  const xappFound = await req.db(`
-    SELECT
-      application_xapp_identifier,
-      application_xapp_url
-    FROM applications WHERE application_xapp_identifier = :xapp
-  `, {xapp: req.params.app})
+  try {
+    let xummUa = false
 
-  const xappErrorPageParams = {
-    ott: req?.query?.xAppToken,
-    module: 'xapps',
-    mode: req.config.mode,
-    xAppIdentifier: req?.params?.app,
-    headers: req.headers,
-    xappFound,
-    // loadedInXumm: Object.keys(headers).length > 0
-  }
+    if (typeof req.headers['user-agent'] !== 'undefined') {
+      if (req.headers['user-agent'].slice(':')[0] === 'xumm/xapp') {
+        xummUa = true
+      }
+    }
 
-  log({xappErrorPageParams})
+    if (!xummUa) {
+      throw Error('Invalid xApp invoke')
+    }
 
-  if (Array.isArray(xappFound) && xappFound.length > 0) {
-    if (typeof xappFound[0].application_xapp_url === 'string' && xappFound[0].application_xapp_url !== '') {
-      return res.status(301).redirect(
-        xappFound[0].application_xapp_url
-          + (xappFound[0].application_xapp_url.match(/\?/) ? '&' : '?')
-          + 'xAppToken=' + 'xxxxxx'
-      )
+    const xappFound = await req.db(`
+      SELECT
+        application_xapp_identifier,
+        application_xapp_url
+      FROM applications WHERE application_xapp_identifier = :xapp
+    `, {xapp: req.params.app})
+
+    const xappErrorPageParams = {
+      ott: req?.query?.xAppToken,
+      module: 'xapps',
+      mode: req.config.mode,
+      xAppIdentifier: req?.params?.app,
+      headers: req.headers,
+      xappFound,
+      // loadedInXumm: Object.keys(headers).length > 0
+    }
+
+    log({xappErrorPageParams})
+
+    if (Array.isArray(xappFound) && xappFound.length > 0) {
+      if (typeof xappFound[0].application_xapp_url === 'string' && xappFound[0].application_xapp_url !== '') {
+        return res.status(301).redirect(
+          xappFound[0].application_xapp_url
+            + (xappFound[0].application_xapp_url.match(/\?/) ? '&' : '?')
+            + 'xAppToken=' + 'xxxxxx'
+        )
+      } else {
+        return res.render('xapps/index.html', {
+          ...xappErrorPageParams,
+          error: { code: 500, desc: 'Not configured' }
+        })  
+      }
     } else {
       return res.render('xapps/index.html', {
         ...xappErrorPageParams,
-        error: { code: 500, desc: 'Not configured' }
-      })  
+        error: { code: 404, desc: 'Not found' }
+      })
     }
-  } else {
-    return res.render('xapps/index.html', {
-      ...xappErrorPageParams,
-      error: { code: 404, desc: 'Not found' }
-    })
+  } catch (e) {
+    log({e})
+    res.status(404).render('500', { error: e.message })
   }
 }
